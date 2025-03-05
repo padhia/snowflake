35a22000f022a293c81e75bc68ad1a72da5c7106
Paresh Adhia <padhia@gmail.com>
2025-03-05 13:29:37 -0500

    snowflake-cli -> 3.4.1

--- flake.nix
+++ flake.nix
@@ -6,28 +6,16 @@
 
   outputs = { self, nixpkgs, flake-utils }:
   let
-    typer-fix = py-final: py-prev: {
-      typer125 = py-prev.typer.overridePythonAttrs (old: rec {
-        pname = "typer";
-        version = "0.12.5";
-        src =   py-final.fetchPypi {
-          inherit pname version;
-          hash = "sha256-9ZLwib7cyOwbl0El1khRApw7GvFF8ErKZNaUEPDJtyI=";
-        };
-        doCheck = false;  # fails on darwin
-      });
-    };
-
     pyOverlay = py-final: py-prev: {
       snowflake-snowpark-python = py-final.callPackage ./snowflake-snowpark-python.nix {};
       snowflake-core = py-final.callPackage ./snowflake-core.nix {};
-      snowflake-cli  = py-final.callPackage ./snowflake-cli.nix { typer = py-final.typer125; };
+      snowflake-cli  = py-final.callPackage ./snowflake-cli.nix {};
     };
 
     overlays.default = final: prev: {
       inherit (final.python311Packages) snowflake-cli;
       snowsql = prev.callPackage ./snowsql.nix {};
-      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [ typer-fix pyOverlay ];
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [ pyOverlay ];
     };
 
     eachSystem = flake-utils.lib.eachDefaultSystem (system:
--- snowflake-cli.nix
+++ snowflake-cli.nix
@@ -27,14 +27,14 @@
 
 buildPythonPackage rec {
   pname = "snowflake-cli";
-  version = "3.2.2";
+  version = "3.4.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "snowflakedb";
     repo = "snowflake-cli";
     rev = "refs/tags/v${version}";
-    hash = "sha256-G1jX9v0OliDMqojNu3ZSGVfvH74OlIsIQjRiLprrxYA=";
+    hash = "sha256-W+/RW/oTFi7NyJZXlK9Yd3Gggmm+PtmOawWFSrK+SIo=";
   };
 
   disabled = pythonOlder "3.10";
