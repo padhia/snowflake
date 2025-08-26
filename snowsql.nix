6069af8dfbb5dec49c8513f0f85691f4eaacacb9
Paresh Adhia <padhia@gmail.com>
2025-08-25 21:34:43 -0400

    snowsql -> 1.4.4

--- snowsql.nix
+++ snowsql.nix
@@ -4,22 +4,35 @@
 , rpmextract
 , patchelf
 , makeWrapper
+, libz
 , openssl
 , libxcrypt-legacy
 }:
 
 stdenv.mkDerivation rec {
   pname = "snowsql";
-  version = "1.4.3";
+  version = "1.4.4";
 
-  src = fetchurl {
-    url = "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.4/linux_x86_64/snowflake-snowsql-${version}-1.x86_64.rpm";
-    sha256 = "sha256-8+LG7KkkO5kYPHBpMnXXCXbZmJpc/mS/9JujHShw/ZU=";
-  };
+  src = let
+    sources = {
+      x86_64-linux = {
+        url = "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.4/linux_x86_64/snowflake-snowsql-${version}-1.x86_64.rpm";
+        sha256 = "sha256-rLUOMZySCkc/eM4T0A29ULvaZvqOuep2ycFNmKtv6J8=";
+      };
+      aarch64-linux = {
+        url = "https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.4/linux_aarch64/snowflake-snowsql-${version}-1.aarch64.rpm";
+        sha256 = "sha256-Ol+N/trv2ay6j1UAuzwD6IEIk3UNhcZhRfMEBXzsyOs=";
+      };
+    };
+  in fetchurl sources."${stdenv.system}";
 
   nativeBuildInputs = [ rpmextract makeWrapper ];
 
-  libPath = lib.makeLibraryPath [ openssl libxcrypt-legacy ];
+  libPath = lib.makeLibraryPath [
+    libz
+    openssl
+    libxcrypt-legacy
+  ];
 
   buildCommand = ''
     mkdir -p $out/bin/
@@ -43,6 +56,6 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     maintainers = with maintainers; [ andehen padhia ];
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
   };
 }
