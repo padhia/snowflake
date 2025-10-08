056a94f688ac25a40b18def743d775d6238fd7f2
Paresh Adhia <padhia@gmail.com>
2025-10-07 22:05:45 -0400

    snowflake-ml-python -> 1.16.0

--- snowflake-ml-python.nix
+++ snowflake-ml-python.nix
@@ -1,47 +1,48 @@
-{ lib
-, buildPythonPackage
-, pythonRelaxDepsHook
-, fetchPypi
-, pythonOlder
-, setuptools
+{
+  lib,
+  buildPythonPackage,
+  pythonRelaxDepsHook,
+  fetchPypi,
+  pythonOlder,
+  setuptools,
 
-, absl-py
-, anyio
-, cachetools
-, cloudpickle
-, cryptography
-, fsspec
-, importlib-resources
-, numpy
-, packaging
-, pandas
-, pyarrow
-, pydantic
-, pyjwt
-, pytimeparse
-, pyyaml
-, retrying
-, s3fs
-, scikit-learn
-, scipy
-, shap
-, snowflake-connector-python
-, snowflake-core
-, snowflake-snowpark-python
-, sqlparse
-, typing-extensions
-, xgboost
+  absl-py,
+  anyio,
+  cachetools,
+  cloudpickle,
+  cryptography,
+  fsspec,
+  importlib-resources,
+  numpy,
+  packaging,
+  pandas,
+  pyarrow,
+  pydantic,
+  pyjwt,
+  pytimeparse,
+  pyyaml,
+  retrying,
+  s3fs,
+  scikit-learn,
+  scipy,
+  shap,
+  snowflake-connector-python,
+  snowflake-core,
+  snowflake-snowpark-python,
+  sqlparse,
+  typing-extensions,
+  xgboost,
 }:
 
 buildPythonPackage rec {
-  pname     = "snowflake-ml-python";
-  version   = "1.15.0";
+  pname = "snowflake-ml-python";
+  version = "1.16.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "snowflake_ml_python";
     inherit version;
-    hash = "sha256-qlqFsvAtkaLoLsVNv7twO6NlNjzMMXg5Jh5pzQcsiBs=";
+    hash = "sha256-cak58UeWu2eTLcyU9G9EtebgwaDJHmizk9RZHTHMmOo=";
   };
 
   disabled = pythonOlder "3.9";
@@ -79,16 +80,17 @@ buildPythonPackage rec {
     sqlparse
     typing-extensions
     xgboost
-  ] ++ fsspec.optional-dependencies.http
-    ++ snowflake-connector-python.optional-dependencies.pandas;
+  ]
+  ++ fsspec.optional-dependencies.http
+  ++ snowflake-connector-python.optional-dependencies.pandas;
 
   doCheck = false;
 
   meta = with lib; {
-    changelog   = "https://github.com/snowflakedb/snowpark-python/blob/main/CHANGELOG.md";
+    changelog = "https://github.com/snowflakedb/snowpark-python/blob/main/CHANGELOG.md";
     description = "Snowflake Python API for Resource Management";
-    homepage    = "https://docs.snowflake.com/en/developer-guide/snowpark/index";
-    license     = licenses.asl20;
+    homepage = "https://docs.snowflake.com/en/developer-guide/snowpark/index";
+    license = licenses.asl20;
     maintainers = with maintainers; [ padhia ];
   };
 }
