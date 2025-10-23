6c7133d440d58a375e4b8d6cafebc72164b980bd
Paresh Adhia <padhia+git@gmail.com>
2025-10-23 18:26:18 -0400

    snowflake-snowpark-python -> 1.41.0

--- snowflake-snowpark-python.nix
+++ snowflake-snowpark-python.nix
@@ -20,13 +20,13 @@
 
 buildPythonPackage rec {
   pname = "snowflake-snowpark-python";
-  version = "1.40.0";
+  version = "1.41.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit version;
     pname = "snowflake_snowpark_python";
-    hash = "sha256-8J+RlDMxf1MkDPf4ERa/x4i+3VSvywL1/3vKvnvKuag=";
+    hash = "sha256-GckDVOsQPDfGUC5biAtHI122q7D6wZEMAiqph0AzF4U=";
   };
 
   disabled = pythonOlder "3.9" || pythonAtLeast "3.14";
